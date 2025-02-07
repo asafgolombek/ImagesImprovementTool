6a4dcc7dd854d008edf4141f480ead576bc993f2
AsafGolombek <asafgolombek@gmail.com>
2025-02-07 12:57:38 +0200

    basic

--- ImagesImprovementTool/Utilitis/CmdArgumentsService.py
+++ ImagesImprovementTool/Utilitis/CmdArgumentsService.py
@@ -3,10 +3,10 @@ from Consts import FilesConts
 class CmdArgumentsService:
     def __init__(self):
         self.parser = argparse.ArgumentParser(description="app cmd line arguments")
-        self.parser.add_argument("configuration_file", help="Path to the configuration file", default=FilesConts.default_configuration_file)
+        self.parser.add_argument("--configuration_file", help="Path to the configuration file", default=FilesConts.default_configuration_file)
         self.args = self.parser.parse_args()
 
-    def GetConfigurationFilePath(self):
+    def getConfigurationFilePath(self):
         return self.args.configuration_file
 
 
